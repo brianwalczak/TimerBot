80aad7ab48aacb869fe38fa6755b628e3ec5ff63
Brian <72092838+BrianWalczak@users.noreply.github.com>
2026-05-26 17:36:35 -0500

    Warn users of expired session, pass cache to handler

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,7 +4,33 @@ const { RateLimiter, Cache } = require('../ipc.js');
 module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
+		const replied = interaction.replied || interaction.deferred;
 		if (interaction.user.bot) return;
+
+		async function validateFlow(flow) {
+			if (flow != null && !isNaN(flow)) { // If flow is a number, it is a cache ID
+				const cache = await Cache.getCache(flow);
+
+				// Expired session!
+				if (!cache) {
+					await interaction[replied ? 'followUp' : 'reply']({
+						content: "⚠️ **Whoops!** Looks like your request expired. Please try running this command again.",
+						flags: MessageFlags.Ephemeral
+					});
+
+					return { success: false, cache: null };
+				}
+
+				// User ID mismatch!
+				if (cache.userId != null && cache.userId.toString() !== interaction.user.id.toString()) {
+					return { success: false, cache: cache }; // silent failure, somebody is hijacking somebody else's interaction xD
+				}
+
+				return { success: true, cache: cache };
+			}
+
+			return { success: true, cache: null };
+		}
 
 		// Handle buttons when provided (if handler exists)
 		if (interaction.isButton()) {
@@ -12,12 +38,10 @@ module.exports = {
 			const handler = interaction.client.buttons.get(base);
 			if (handler) {
 				try {
-					if(flow != null && !isNaN(flow)) { // If flow is a number, it is a cache ID
-						const cache = await Cache.getCache(flow);
-						if (!cache || (cache.userId != null && cache.userId.toString() !== interaction.user.id.toString())) return;
-					}
+					const validate = await validateFlow(flow);
+					if (!validate.success) return;
 
-					await handler(interaction);
+					await handler(interaction, validate.cache);
 				} catch (err) {
 					console.error('[ERROR] An error occurred while handling a button: ', err);
 					
@@ -37,12 +61,10 @@ module.exports = {
 			const handler = interaction.client.modals.get(base);
 			if (handler) {
 				try {
-					if(flow != null && !isNaN(flow)) { // If flow is a number, it is a cache ID
-						const cache = await Cache.getCache(flow);
-						if (!cache || (cache.userId != null && cache.userId.toString() !== interaction.user.id.toString())) return;
-					}
+					const validate = await validateFlow(flow);
+					if (!validate.success) return;
 
-					await handler(interaction);
+					await handler(interaction, validate.cache);
 				} catch (err) {
 					console.error('[ERROR] An error occurred while handling a modal: ', err);
 
@@ -61,12 +83,10 @@ module.exports = {
 			const handler = interaction.client.menus.get(base);
 			if (handler) {
 				try {
-					if(flow != null && !isNaN(flow)) { // If flow is a number, it is a cache ID
-						const cache = await Cache.getCache(flow);
-						if (!cache || (cache.userId != null && cache.userId.toString() !== interaction.user.id.toString())) return;
-					}
+					const validate = await validateFlow(flow);
+					if (!validate.success) return;
 
-					await handler(interaction);
+					await handler(interaction, validate.cache);
 				} catch (err) {
 					console.error('[ERROR] An error occurred while handling a select menu: ', err);
 
@@ -98,8 +118,6 @@ module.exports = {
 		try {
 			await command.run(interaction.client, interaction);
 		} catch (error) {
-			const replied = interaction.replied || interaction.deferred;
-			
 			console.error(`[ERROR] An error occurred while executing the command "${interaction.commandName}": `, error);
 			await interaction[replied ? 'followUp' : 'reply']({
 				content: "❌ There was an error executing this command! Please try again later.",
